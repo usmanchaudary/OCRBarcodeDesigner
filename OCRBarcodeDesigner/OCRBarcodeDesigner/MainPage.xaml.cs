4303677175c9fe046c3a392f3614e9b9205a15bd
Shehroze Malik <shehroze@innidata.com>
2022-01-16 22:31:48 -0500

    added ocr funct

--- OCRBarcodeDesigner/OCRBarcodeDesigner/MainPage.xaml.cs
+++ OCRBarcodeDesigner/OCRBarcodeDesigner/MainPage.xaml.cs
@@ -2,9 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using ZXing.Mobile;
 
@@ -12,6 +15,8 @@ namespace OCRBarcodeDesigner
 {
     public partial class MainPage : ContentPage
     {
+        public string PhotoPath { get; private set; }
+
         public MainPage()
         {
             InitializeComponent();
@@ -61,5 +66,60 @@ namespace OCRBarcodeDesigner
 
             DisplayAlert("", msg, "Ok");
         }
+
+        private async void OCR_Test_Clicked(object sender, EventArgs e)
+        {
+            TakePhotoAsync();
+
+        }
+        async Task TakePhotoAsync()
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+                using (WebClient wc = new WebClient())
+                {
+                    wc.Headers["apikey"] = "a4028b8a1188957";
+                    var resp = wc.UploadFile("https://api.ocr.space/parse/image", PhotoPath);
+                    var str = System.Text.Encoding.Default.GetString(resp);
+                    var text = OcrResult.FromJson(str).ParsedResults.First().ParsedText;
+                    Console.WriteLine(text);
+
+
+                   await DisplayAlert("", text, "Ok");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature is not supported on the device
+            }
+            catch (PermissionException pEx)
+            {
+                // Permissions not granted
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+        async Task LoadPhotoAsync(FileResult photo)
+        {
+            // canceled
+            if (photo == null)
+            {
+                PhotoPath = null;
+                return;
+            }
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+
+            PhotoPath = newFile;
+        }
+
     }
 }
---
+++ OCRBarcodeDesigner/OCRBarcodeDesigner/OcrResult.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCRBarcodeDesigner
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class OcrResult
+    {
+        [JsonProperty("ParsedResults")]
+        public List<ParsedResult> ParsedResults { get; set; }
+
+        [JsonProperty("OCRExitCode")]
+        public long OcrExitCode { get; set; }
+
+        [JsonProperty("IsErroredOnProcessing")]
+        public bool IsErroredOnProcessing { get; set; }
+
+        [JsonProperty("ProcessingTimeInMilliseconds")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long ProcessingTimeInMilliseconds { get; set; }
+
+        [JsonProperty("SearchablePDFURL")]
+        public string SearchablePdfurl { get; set; }
+    }
+
+    public partial class ParsedResult
+    {
+        [JsonProperty("TextOverlay")]
+        public TextOverlay TextOverlay { get; set; }
+
+        [JsonProperty("TextOrientation")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long TextOrientation { get; set; }
+
+        [JsonProperty("FileParseExitCode")]
+        public long FileParseExitCode { get; set; }
+
+        [JsonProperty("ParsedText")]
+        public string ParsedText { get; set; }
+
+        [JsonProperty("ErrorMessage")]
+        public string ErrorMessage { get; set; }
+
+        [JsonProperty("ErrorDetails")]
+        public string ErrorDetails { get; set; }
+    }
+
+    public partial class TextOverlay
+    {
+        [JsonProperty("Lines")]
+        public List<object> Lines { get; set; }
+
+        [JsonProperty("HasOverlay")]
+        public bool HasOverlay { get; set; }
+
+        [JsonProperty("Message")]
+        public string Message { get; set; }
+    }
+
+    public partial class OcrResult
+    {
+        public static OcrResult FromJson(string json) => JsonConvert.DeserializeObject<OcrResult>(json, OCRBarcodeDesigner.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+}
